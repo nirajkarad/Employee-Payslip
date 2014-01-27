89acccffd5ea3b51696b033614578030558f0192
Niraj Karad <nirajk@glam.com>
2014-01-27 12:02:47 +0530

    Adding Serialization & Deserialization test cases

--- src/test/java/com/project/test/resource/TestEmployee.java
+++ src/test/java/com/project/test/resource/TestEmployee.java
@@ -1,4 +1,4 @@
- /*
+/*
 
  * Copyright 2010-2013 Ning, Inc.
 
@@ -29,11 +29,15 @@
  */
 package com.project.test.resource;
 
-
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.google.common.collect.ImmutableMap;
 import com.project.employee.Employee;
+import com.project.modules.YearMonthKeyDeserializer;
+import java.io.IOException;
 import java.util.Map;
+import org.joda.time.YearMonth;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -42,32 +46,33 @@ public class TestEmployee
 {
     private Employee emp;
     private Map<String, Integer> breakup;
-    
+
     @BeforeClass
     public void setup()
     {
         emp = new Employee(1, "fname", "lname", "email");
-        breakup = ImmutableMap.of("Basic", 5000 , "HRA", 2000, "Tax", 756);
+        breakup = ImmutableMap.of("Basic", 5000, "HRA", 2000, "Tax", 756);
     }
-    
+
     @Test
     public void TestaddBreakupYearMonth()
     {
         emp.addBreakupYearMonth(breakup, 2014, 1);
     }
-    
-    @Test(dependsOnMethods="TestaddBreakupYearMonth")
+
+    @Test(dependsOnMethods = "TestaddBreakupYearMonth")
     public void TestGetPaySlip()
     {
-        Map<String, Integer> sal =emp.getPayslipForSpecificMonth(2014, 1);
+        Map<String, Integer> sal = emp.getPayslipForSpecificMonth(2014, 1);
         Assert.assertNotNull(sal);
         Assert.assertEquals(Integer.valueOf(5000), sal.get("Basic"));
         Assert.assertEquals(Integer.valueOf(2000), sal.get("HRA"));
         Assert.assertEquals(Integer.valueOf(756), sal.get("Tax"));
     }
-    
+
     @Test
-    public void testSameEmployee(){
+    public void testSameEmployee()
+    {
         Employee emp1 = new Employee(1, "first", "lastname", "first@glam.com");
         Employee emp2 = new Employee(1, "first", "lastname", "first@glam.com");
         Employee emp3 = new Employee(2, "second", "secondname", "second@glam.com");
@@ -76,5 +81,25 @@ public class TestEmployee
         Assert.assertEquals(emp2, emp1);
         Assert.assertNotEquals(emp1, emp3);
         Assert.assertNotEquals(emp2, emp3);
+    }
+
+    @Test
+    public void testEmployeeSerialization() throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Assert.assertEquals(mapper.writeValueAsString(new Employee(1, "fname", "lname", "email")), "{\"id\":1,\"firstName\":\"fname\",\"lastName\":\"lname\",\"email\":\"email\",\"pay_slip\":{}}");
+    }
+
+    @Test
+    public void testEmployeeDeserialization() throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JodaModule().addKeyDeserializer(YearMonth.class, new YearMonthKeyDeserializer()));
+        Employee employee = mapper.readValue("{\"id\":1,\"firstName\":\"fname\",\"lastName\":\"lname\",\"email\":\"email\"}", Employee.class);
+        Assert.assertNotNull(employee);
+        Assert.assertEquals(1, employee.getId());
+        Assert.assertEquals("fname", employee.getFirstName());
+        Assert.assertEquals("lname", employee.getLastName());   
+        Assert.assertEquals("email", employee.getEmail());
     }
 }
